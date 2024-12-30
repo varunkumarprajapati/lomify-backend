d5df784b51db257032df541c7dc703d17b93139f
VarunKumarPrajapati <varunkumarprajapati57@gmail.com>
2024-12-30 11:52:06 +0530

    Config CORS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("./database/db.js");
 require("dotenv").config();
 require("express-async-errors");
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
@@ -10,6 +11,12 @@ const verificationRoute = require("./routes/verificationRoute.js");
 const error = require("./middleware/error.js");
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:8000", "https://lomify-backend.vercel.app"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Home page");
